02d9c2a1b5ceeb6b8919005c1a93d03e98188259
hauhc1203 <hauhc1203@gmail.com>
2022-05-09 12:41:07 +0700

    Khai bao va su dung bien

---
+++ Week1/src/khaoBaoSDBien.java
@@ -0,0 +1,17 @@
+public class khaoBaoSDBien {
+    public static void main(String[] args) {
+        int i=10;
+        float f=20.5f;
+        double d=20.5d;
+        boolean b=true;
+        char c='a';
+        String s="Ha noi";
+        System.out.println("So nguyen i co gia tri la: "+i);
+        System.out.println("So thuc f co gia tri la: "+f);
+        System.out.println("So thuc d co gia tri la: "+d);
+        System.out.println("bien logic b co gia tri la: "+b);
+        System.out.println("ky ty a co gia tri la: "+c);
+        System.out.println("chuoi s co gia tri la: "+s);
+
+    }
+}
