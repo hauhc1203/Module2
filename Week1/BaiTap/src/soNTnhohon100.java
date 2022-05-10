a9b3a25994e79a1d0c3f8431f3b04282560ba0cb
hauhc1203 <hau.hc1203@gmail.com>
2022-05-10 14:05:46 +0700

    so nguyen to <100

--- Week1/BaiTap/src/soNTnhohon100.java
+++ Week1/BaiTap/src/soNTnhohon100.java
@@ -1,2 +1,10 @@
-package PACKAGE_NAME;public class soNTnhohon100 {
+public class soNTnhohon100 {
+    public static void main(String[] args) {
+        System.out.println("Cac so nguyen to nho hon 100 la ");
+        for(int i=2;i<100;i++){
+            if (nSoNTdautien.isSoNT(i)){
+                System.out.print(i+" ");
+            }
+        }
+    }
 }
