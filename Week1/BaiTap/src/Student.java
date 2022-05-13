27329fc6d5fce750729cf1b5d1e94e0340da139e
hauhc1203 <hau.hc1203@gmail.com>
2022-05-13 16:16:06 +0700

    xay dung lop chi ghi

---
+++ Week1/BaiTap/src/Student.java
@@ -0,0 +1,23 @@
+public class Student {
+    private String name="John";
+    private String lop="C0322G1";
+
+    public Student() {
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getLop() {
+        return lop;
+    }
+
+    public void setLop(String lop) {
+        this.lop = lop;
+    }
+}
---
+++ Week1/BaiTap/src/Test.java
@@ -0,0 +1,11 @@
+public class Test {
+    public static void main(String[] args) {
+        Student s1=new Student();
+        System.out.println(s1.getName());
+        System.out.println(s1.getLop());
+        s1.setName("Hau");
+        s1.setLop("C03");
+        System.out.println(s1.getName());
+        System.out.println(s1.getLop());
+    }
+}
