fcceb3965ea904f9ce7c2a4aa666b739c5c2e3bd
hauhc1203 <hau.hc1203@gmail.com>
2022-05-17 15:17:28 +0700

    th3

---
+++ Week2/ThucHanhW2/src/th3/CircleComparator.java
@@ -0,0 +1,17 @@
+package th3;
+
+import th2.Circle;
+
+import java.util.Comparator;
+
+public class CircleComparator implements Comparator<Circle>{
+
+
+
+    @Override
+    public int compare(Circle c1, Circle c2) {
+        if (c1.getRadius() > c2.getRadius()) return 1;
+        else if (c1.getRadius() < c2.getRadius()) return -1;
+        else return 0;
+    }
+}
