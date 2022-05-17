98bdd966a57f9e39dbf04b24479818562aec09a3
hauhc1203 <hau.hc1203@gmail.com>
2022-05-17 11:16:34 +0700

    triagle

---
+++ Week2/BaiTapW2/src/Shape.java
@@ -0,0 +1,42 @@
+public class Shape {
+    public String color;
+    public boolean filled;
+
+    public Shape() {
+        this.color="green";
+        this.filled=true;
+    }
+
+    public Shape(String color, boolean filled) {
+        this.color = color;
+        this.filled = filled;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public boolean isFilled() {
+        return filled;
+    }
+
+    public void setFilled(boolean filled) {
+        this.filled = filled;
+    }
+
+    @Override
+    public String toString() {
+        String result="A Shape with color of "+this.color+" and";
+        if (this.filled){
+            result+=" filled";
+
+        }else {
+            result+=" not filled";
+        }
+        return result;
+    }
+}
---
+++ Week2/BaiTapW2/src/TestTriagle.java
@@ -0,0 +1,6 @@
+public class TestTriagle {
+    public static void main(String[] args) {
+        Triangle triangle=new Triangle(7,8,9);
+        System.out.println(triangle);
+    }
+}
