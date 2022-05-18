74cc1971d881ec9f17412419941bf2b8ef976cc1
hauhc1203 <hau.hc1203@gmail.com>
2022-05-18 17:26:38 +0700

    refactor tach method

---
+++ Week2/ThucHanhW2/src/tachMethod/Cylinder.java
@@ -0,0 +1,18 @@
+package tachMethod;
+
+public class Cylinder {
+    public  double getVolume(double radius, double height){
+        double volume=perimeter(radius)*height+2*baseArea(radius);
+        return volume;
+    }
+
+    public double baseArea(double radius){
+        return  Math.PI * radius * radius;
+    }
+
+    public double perimeter(double radius){
+        return 2 * Math.PI  * radius;
+    }
+
+
+}
