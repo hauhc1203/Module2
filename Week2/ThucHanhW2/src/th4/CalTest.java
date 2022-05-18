e086e4de42d72fb508a294d4d7220a66a5eed85f
hauhc1203 <hau.hc1203@gmail.com>
2022-05-18 17:19:08 +0700

    refactor tach bien

--- Week2/ThucHanhW2/src/th4/CalTest.java
+++ Week2/ThucHanhW2/src/th4/CalTest.java
@@ -3,6 +3,6 @@ package th4;
 public class CalTest {
     public static void main(String[] args) {
         Calculator calculator =new Calculator();
-        System.out.println(calculator.calculate(3,4,'+'));;
+        System.out.println(calculator.calculate(3,4,'+'));
     }
 }
