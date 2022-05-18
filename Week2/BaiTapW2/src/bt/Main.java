d7da9abb2d62f901d2a4fe779f04d4a2441ad665
hauhc1203 <hau.hc1203@gmail.com>
2022-05-18 14:28:27 +0700

    update

--- Week2/BaiTapW2/src/bt/Main.java
+++ Week2/BaiTapW2/src/bt/Main.java
@@ -1,25 +1,41 @@
 package bt;
 
+import java.util.Random;
+
 public class Main {
     public static void main(String[] args) {
+        Shape shapes[]=new Shape[5];
+        shapes[0]=new ResizeRectangle(20,30);
+        shapes[1]=new ResizeSquare(40);
+        shapes[2]=new CircleResize(30);
+        shapes[3]=new ResizeRectangle(10,20);
+        shapes[4]=new ResizeSquare(30);
 
-        CircleResize circleResize=new CircleResize(30);
 
-        System.out.println(circleResize);
-        circleResize.reSize(150);
-        System.out.println(circleResize);
+        //mang truoc resize
+        for (Shape s:shapes){
+            System.out.println(s);
+        }
+        System.out.println("--------------------------------------------");
+        //in mang hinh sau resize
+        for (Shape s:shapes){
+            Random r=new Random();
+            int scale=r.nextInt(100)+100;
+
+            System.out.println("Ty le tang "+scale+" %");
+            if(s instanceof ResizeSquare){
+                ((ResizeSquare)s).reSize(scale);
+            }else if(s instanceof  CircleResize){
+                ((CircleResize)s).reSize(scale);
+            }else if (s instanceof ResizeRectangle){
+                ((ResizeRectangle)s).reSize(scale);
+            }
+            System.out.println(s);
+
+        }
 
 
-        ResizeRectangle rect=new ResizeRectangle(10,20);
-        System.out.println(rect);
 
-        rect.reSize(50);
-        System.out.println(rect);
-
-        ResizeSquare resizeSquare=new ResizeSquare(100);
-        System.out.println(resizeSquare);
-        resizeSquare.reSize(30);
-        System.out.println(resizeSquare);
 
     }
 }
