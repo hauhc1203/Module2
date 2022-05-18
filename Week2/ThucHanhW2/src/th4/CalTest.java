7a3a4176597a4c934a9e530f0b48ec29009fcf50
hauhc1203 <hau.hc1203@gmail.com>
2022-05-18 17:11:17 +0700

    refactor

---
+++ Week2/ThucHanhW2/src/th4/CalTest.java
@@ -0,0 +1,8 @@
+package th4;
+
+public class CalTest {
+    public static void main(String[] args) {
+        Calculator calculator =new Calculator();
+        System.out.println(calculator.calculate(3,4,'+'));;
+    }
+}
