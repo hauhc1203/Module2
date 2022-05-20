7cedd34fde788c4a6ad13ae49bb831c342db350c
hauhc1203 <hau.hc1203@gmail.com>
2022-05-20 16:10:22 +0700

    linked list

---
+++ Week2/ThucHanhW2/src/LinkedList/TestMyLinkedList.java
@@ -0,0 +1,20 @@
+package LinkedList;
+
+public class TestMyLinkedList {
+    public static void main(String[] args) {
+        System.out.println("/=/=/=/= TESTING /=/=/=/=");
+        MyLinkedList ll = new MyLinkedList(10);
+        ll.addFirst(11);
+        ll.addFirst(12);
+        ll.addFirst(13);
+        ll.add(5,3);
+        ll.add(2,9);
+        ll.add(4,1);
+        ll.printList();
+        System.out.println("------------------------------------------------");
+        System.out.println(ll.get(3) );
+
+
+
+    }
+}
