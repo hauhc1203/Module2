6edbaba45bb3358ec6c31ee77f19039c93bbabeb
hauhc1203 <hau.hc1203@gmail.com>
2022-05-24 10:48:36 +0700

    tree map

--- Week3/BT/src/treeMap/TreeMap.java
+++ Week3/BT/src/treeMap/TreeMap.java
@@ -4,12 +4,26 @@ import java.util.Locale;
 import java.util.Map;
 
 public class TreeMap {
+    public  static  Map.Entry coTrongMap(String key, Map<String,Integer> map,Map.Entry entry1){
+
+        for (Map.Entry< String,Integer> entry : map.entrySet()) {
+            String key1 = entry.getKey();
+            if (key.equals(key1)) {
+                entry1=entry;
+                return entry1;
+            }
+        }
+        return entry1;
+    }
+
     public static void main(String[] args) {
-        String str="       xin chao haha haha haha haha xin xin chao xin haha     ";
-        Map<String,Integer> map=new java.util.TreeMap<String, Integer>();
+        String str=" d d d waa sadwfas wa ef aa aw wq aa a a a a a a a a  a a  dsgs hdh jtkghc k hj rhh xin chao haha haha haha haha xin xin chao xin haha     ";
+        Map<String,Integer> map=new java.util.TreeMap<>();
         int start=0;
         int end=0;
-        int c=0;
+
+        Map.Entry<String,Integer> entry2 = null;
+
         for (int i = 0; i <str.length()-1 ; i++) {
             if(str.charAt(i)==' '&& str.charAt(i+1)!=' '){
                 start=i+1;
@@ -19,21 +33,12 @@ public class TreeMap {
             }
             if (start!=0&&end!=0){
                 String key=str.substring(start,end);
-                if (c==0){
+                entry2=coTrongMap(key,map,entry2);
+                if (entry2==null){
                     map.put(key,1);
-                    c++;
                 }else {
-                    for (Map.Entry< String,Integer> entry : map.entrySet()) {
-                        String key1 = entry.getKey();
-                        if(key.equals(key1)){
-
-                            entry.setValue(entry.getValue()+1);
-                            break;
-                        }
-//                        else {
-//                            map.put(key,1);
-//                        }
-                    }
+                    entry2.setValue(entry2.getValue() + 1);
+                    entry2=null;
                 }
                 start=0;
                 end=0;
