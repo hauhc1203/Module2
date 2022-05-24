ce565e5bc8be98b5ee413f17c0a95356dae8d76f
hauhc1203 <hau.hc1203@gmail.com>
2022-05-24 14:24:52 +0700

    map

---
+++ Week3/TH/src/hashMap_linkedMap_treeMap/TestMap.java
@@ -0,0 +1,42 @@
+package hashMap_linkedMap_treeMap;
+
+import java.util.HashMap;
+import java.util.LinkedHashMap;
+import java.util.Map;
+import java.util.TreeMap;
+
+public class TestMap {
+    public static void main(String[] args) {
+
+        Map<String, Integer> hashMap = new HashMap<>();
+        hashMap.put("Lewis", 29);
+        hashMap.put("Smith", 30);
+        hashMap.put("Anderson", 31);
+
+        hashMap.put("Cook", 29);
+        System.out.println("Display entries in HashMap");
+        for (Map.Entry<String,Integer>entry: hashMap.entrySet()){
+            System.out.println(entry.getKey());
+
+            System.out.println(entry.getValue());
+            System.out.println("------------------");
+        }
+        System.out.println(hashMap + "\n");
+        System.out.println("-----------------------------------------------");
+        Map<String, Integer> treeMap = new TreeMap<>(hashMap);
+        System.out.println("Display entries in ascending order of key");
+        System.out.println(treeMap);
+
+
+        System.out.println("-----------------------------------------------");
+
+        Map<String, Integer> linkedHashMap = new LinkedHashMap<>(16, 0.75f, true);
+        linkedHashMap.put("Smith", 30);
+        linkedHashMap.put("Anderson", 31);
+        linkedHashMap.put("Lewis", 29);
+        linkedHashMap.put("Cook", 29);
+        System.out.println("\nThe age for " + "Lewis is " + linkedHashMap.get("Lewis"));
+
+
+    }
+}
