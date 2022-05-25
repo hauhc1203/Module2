96638a89de4ff0e81f9b06b9979fc69e109c6968
hauhc1203 <hau.hc1203@gmail.com>
2022-05-25 14:08:12 +0700

    sub string

---
+++ Week3/BT/src/subString/SubString.java
@@ -0,0 +1,38 @@
+package subString;
+
+import java.util.LinkedList;
+import java.util.Scanner;
+
+public class SubString {
+
+
+    static void subString(String str){
+        LinkedList<Character> subStringMax=new LinkedList<>();
+        for (int i=0;i<str.length();i++){
+            LinkedList<Character> list=new LinkedList<>();
+            list.add(str.charAt(i));
+            for (int j=i+1;j<str.length()-1;j++){
+                if (str.charAt(j)>list.getLast()){
+                    list.add(str.charAt(j));
+                }
+            }
+            if (list.size()>subStringMax.size()){
+                subStringMax.clear();
+                subStringMax.addAll(list);
+            }
+            list.clear();
+        }
+        for (Character c:subStringMax
+             ) {
+            System.out.print(c);
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Nhap vao 1 chuoi");
+        String str =sc.nextLine();
+        subString(str);
+
+    }
+}
