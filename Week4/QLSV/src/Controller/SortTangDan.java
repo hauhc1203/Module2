b81f9857d3d19c0b0c4d692a6950ecfd8cdee9e8
hauhc1203 <hau.hc1203@gmail.com>
2022-05-31 20:49:17 +0700

    Thi Thu

---
+++ Week4/QLSV/src/Controller/SortTangDan.java
@@ -0,0 +1,14 @@
+package Controller;
+
+import Model.Student;
+
+import java.util.Comparator;
+
+public class SortTangDan implements Comparator<Student> {
+    @Override
+    public int compare(Student o1, Student o2) {
+        if (o1.getDiemTB()>o2.getDiemTB()){
+            return 1;
+        }else return -1;
+    }
+}
